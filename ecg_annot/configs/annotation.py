1e84a664fcb38d9328cb2c685a9a3ec1097b69f7
willxxy <willhan327@outlook.com>
2025-11-23 19:59:56 -0500

    configs start

---
+++ ecg_annot/configs/annotation.py
@@ -0,0 +1,19 @@
+QRS_GRAPH = {
+    "QRS",
+    "Pacing",
+    "Axis",
+    "Lead reversal",
+    "Rate",
+    "Amplitude",
+    "Morphology",
+    "Preexcitation",
+    "AP",
+    "Duration",
+    "<110",
+    ">120",
+    "110-120",
+    "rSR'",
+}
+
+if __name__ == "__main__":
+    print(QRS_GRAPH)
